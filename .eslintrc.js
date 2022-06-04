8b84435905d62f62ea634a334630b717864ad7c8
Caio Damasceno <83442511+CaioDamascenoAlves@users.noreply.github.com>
2022-06-04 12:51:04 -0300

    Add Files
    First Form

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+	'no-unused-vars' : 'off',
+	'no-undef' : 'off',
+	'no-mixed-spaces-and-tabs' : 'off',
+	'vue/no-unused-components' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
